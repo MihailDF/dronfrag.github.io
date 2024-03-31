199a14e7d2a6ec46313f685a207575088cfff2cf
MihailDF <161213420+MihailDF@users.noreply.github.com>
2024-03-31 16:08:34 +0300

    Update logics.js
    создал переменные кнопки, main, список для поиска каких-либо товаров

--- logics.js
+++ logics.js
@@ -1,1 +1,13 @@
-
+function scrollDown() { //функция для скрола
+    window.scrollTo({top: window.innerHeight, behavior: 'smooth' });
+}
+let search_button = document.querySelector(".button-search")
+let main = document.querySelector(".main")
+let iphone = document.querySelector(".iphone")
+console.log(search_button)
+let ser_numbers_list = ["1","2","3","4"] // 1 - первый телефон, 2 - компьютер, 3- телефоны, 4- холодильник
+search_button.addEventListener('click', function(){
+    let ser_number =document.querySelector("input")
+    main.style.display = none
+    console.log("pifjwijffjwiofj")
+})
