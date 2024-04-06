00cfb9a91d2a33fa67769fc95e063b2d43054608
MihailDF <161213420+MihailDF@users.noreply.github.com>
2024-04-07 00:06:23 +0300

    Update logics.js
    добавил работу кнопки 'поиск', добавил реакцию на кнопку "купить", не смог сделать реакцию на выбранную категорию из списка

--- logics.js
+++ logics.js
@@ -4,10 +4,45 @@ function scrollDown() { //функция для скрола
 let search_button = document.querySelector(".button-search")
 let main = document.querySelector(".main")
 let iphone = document.querySelector(".iphone")
-console.log(search_button)
-let ser_numbers_list = ["1","2","3","4"] // 1 - первый телефон, 2 - компьютер, 3- телефоны, 4- холодильник
+let holodos = document.querySelector(".holodos")
+let sigma = document.querySelector(".sigma")
+let pc = document.querySelector(".pc")
+let button_buy = document.querySelector(".button-buy")
+// 1 - первый телефон, 2 - компьютер, 3- сигма телефон, 4- холодильник
 search_button.addEventListener('click', function(){
-    let ser_number =document.querySelector("input")
-    main.style.display = none
-    console.log("pifjwijffjwiofj")
+    let ser_number =document.querySelector("input").value
+    if(ser_number == 1){
+        holodos.style.display = 'none'
+        sigma.style.display = 'none'
+        pc.style.display = 'none'
+    }else{
+        if(ser_number==2){
+            iphone.style.display ="none"
+            holodos.style.display = 'none'
+            sigma.style.display = 'none'
+        }else{
+            if(ser_number==3){
+                iphone.style.display ="none"
+                holodos.style.display = 'none'
+                pc.style.display = 'none'
+            }else{
+                if(ser_number==4){
+                    iphone.style.display ="none"
+                    sigma.style.display = 'none'
+                    pc.style.display = 'none'
+                }else{
+                    alert("Товар не найден")
+                }
+            }
+        }
+    }
+    
 })
+button_buy.addEventListener("click",function(){
+    alert("Товара нет на складе")
+})
+// ('#1').on('change',function(){ не могу понять, как добавить реакцию на выбранный элемент выпадающего списка
+//     if($(this).value() == 1){
+//         alert("телефоны")
+//     }
+// })
